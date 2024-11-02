17e36129c49e222401853fe8404e2f2fa6570bdb
cyclops-psi <cyclops.psi-0b@icloud.com>
2024-11-02 16:57:10 +0300

    Updated scripts

--- scripts/routes.rb
+++ scripts/routes.rb
@@ -1,3 +1,4 @@
 Security Firewalls NLP Containerization Artificial Intelligence
 DevOps Algorithm Agile Work hard in silence, let your success be your noise. Load Testing Testing Data Science RabbitMQ JWT Version Control
 WebSockets Zero Downtime Deployment Neural Networks Distributed Systems Success doesn’t just find you. You have to go out and get it. Backend OAuth Python
+Serverless API Algorithm Version Control Database To be the best, you must be able to handle the worst. Database Indexing Testing DevOps OAuth IoT Act as if what you do makes a difference. It does. JWT Code Review
