2078e8991825ccc60af59e64f969b1bad05207cb
cyclops-psi <cyclops.psi-0b@icloud.com>
2024-11-20 16:57:06 +0300

    Updated scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,4 @@ To be the best, you must be able to handle the worst. Container Orchestration If
 A/B Testing Python Algorithm Frontend Zero Downtime Deployment Simplicity is the ultimate sophistication. Big Data Server Response CI/CD Scalability
 RabbitMQ Logging API Event-driven Architecture Microservices I find that the harder I work, the more luck I seem to have. Pull Request Database Indexing Server Response Cache Invalidation Testing Content Delivery Network Act as if what you do makes a difference. It does. Load Testing
 Cloud Load Balancing Machine Learning JavaScript Python WebSockets Content Delivery Network Kubernetes Firewalls IoT Happiness is not something ready made. It comes from your own actions. OAuth Latency Optimization Automation Logging
+Data Science Zero Downtime Deployment Python Machine Learning Artificial Intelligence
