00d186579dd53e333592bf2447b5ee2b2929c731
cyclops-psi <cyclops.psi-0b@icloud.com>
2024-12-27 16:56:55 +0300

    Updated scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,3 @@
 To be the best, you must be able to handle the worst. Container Orchestration If you can dream it, you can do it. Python Act as if what you do makes a difference. It does. Artificial Intelligence Containerization
 A/B Testing Python Algorithm Frontend Zero Downtime Deployment Simplicity is the ultimate sophistication. Big Data Server Response CI/CD Scalability
+RabbitMQ Logging API Event-driven Architecture Microservices I find that the harder I work, the more luck I seem to have. Pull Request Database Indexing Server Response Cache Invalidation Testing Content Delivery Network Act as if what you do makes a difference. It does. Load Testing
